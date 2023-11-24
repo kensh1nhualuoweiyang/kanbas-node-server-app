f3976cd312247e006a8052e29836b24c6434deff
Chen <kensh1nhualuoweiyang@gmail.com>
2023-11-24 15:56:00 -0500

    small fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import session from 'express-session';
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING)
-//mongodb+srv://kensh1nhualuoweiyang:Kensh1nhualuoweiyang@cluster0.zhlknim.mongodb.net/?retryWrites=true&w=majority
+
 const app = express()
 app.use(
     cors({
@@ -26,8 +26,8 @@ const sessionOption = {
     saveUninitialized: false,
 }
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
+    sessionOption.proxy = true;
+    sessionOption.cookie = {
         sameSite: "none",
         secure: true,
     };
